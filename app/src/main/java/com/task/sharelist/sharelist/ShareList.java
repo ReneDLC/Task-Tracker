c7bd65226975f6277f3773084c2e9cf183eb5b0d
JoshuaStrain <Joshua.a.strain@gmail.com>
2018-05-29 23:43:01 -0500

    Project

---
+++ app/src/main/java/com/task/sharelist/sharelist/ShareList.java
@@ -0,0 +1,13 @@
+package com.task.sharelist.sharelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_list);
+    }
+}
