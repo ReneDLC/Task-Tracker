b1d1bdaf06f7d132e40747fc0794041d2f8e2136
Xing <xweng49@gmail.com>
2018-05-30 21:50:46 -0500

    Added Registration Page

--- app/src/main/java/com/task/sharelist/sharelist/ShareList.java
+++ app/src/main/java/com/task/sharelist/sharelist/ShareList.java
@@ -2,12 +2,28 @@ package com.task.sharelist.sharelist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class ShareList extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_list);
+
+        btn = (Button)findViewById(R.id.register);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ShareList.this, RegistrationActivity.class));
+            }
+        });
     }
+
+
+
 }
