5a0d9480acfc628138dd74ad18bf9f22d2a1bf9d
ReneDLC <renedlc1@gmail.com>
2018-06-04 23:33:03 -0500

    Added Alert Dialog

--- app/src/main/java/com/task/sharelist/sharelist/ShareList.java
+++ app/src/main/java/com/task/sharelist/sharelist/ShareList.java
@@ -1,22 +1,21 @@
 package com.task.sharelist.sharelist;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
-import android.util.Log;
+import android.app.AlertDialog;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -75,19 +74,37 @@ public class ShareList extends AppCompatActivity implements View.OnClickListener
         progressDialog.setMessage("Logging in");
         progressDialog.show();
 
+        final CharSequence title, message, ok;
+        title = "Error";
+        message = "Login Was Unsuccessful";
+        ok = "OK";
+
         // logging in
         firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         // successful login, go to profile page
+                        progressDialog.dismiss();
                         if (task.isSuccessful())
                         {
-                            progressDialog.dismiss();
                             finish();
                             startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         }
-
+                        else{
+                            AlertDialog.Builder builder = new AlertDialog.Builder(ShareList.this);
+                            builder.setMessage(title)
+                                    .setTitle(message)
+                                    .setPositiveButton(ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // User clicked OK button
+                                }
+                            })
+                                    .show();
+                            AlertDialog dialog = builder.create();
+                            userName.setText("");
+                            userPassword.setText("");
+                        }
                     }
                 });
 
